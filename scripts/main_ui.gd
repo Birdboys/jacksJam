c8d6ff15214964004085a2bb1c32547f78ed3974
Birdboys <colbyjbird@comcast.net>
2025-10-03 18:02:35 -0600

    basic point and click functions

--- scripts/main_ui.gd
+++ scripts/main_ui.gd
@@ -1,20 +1,53 @@
 extends Control
 
+@onready var roomMargin := $uiMargin/uiCont/topHbox/roomPanel/roomMargin
+
+@onready var descriptionText := $uiMargin/uiCont/botHbox/textPanel/textMargin/descriptionText
 @onready var flashlightButton := $uiMargin/uiCont/botHbox/lightPanel/buttonMargin/flashlightButton
 @onready var mouseLabel := $mouseLabel
-@onready var roomButtons := $uiMargin/uiCont/topHbox/roomPanel/roomMargin/roomTexture/roomButtons
+
+var flashlight_on := false
+var current_room_resource : RoomResource
+var roomButtons
 
 func _ready() -> void:
 	flashlightButton.toggled.connect(toggleFlashlight)
-	getRoomButtons()
+	loadRoom("front_door_test")
 	
 func _process(delta: float) -> void:
 	mouseLabel.global_position = get_global_mouse_position() + Vector2(16, 16)
 	
 func toggleFlashlight(on: bool):
-	if on: return
-	else: return
+	flashlight_on = on
+	if on: 
+		handleFlashlightEvent(current_room_resource.room_flashlight_on_event)
+	else: 
+		handleFlashlightEvent(current_room_resource.room_flashlight_off_event)
 
+func loadRoom(room_name):
+	unloadCurrentRoom()
+	var new_room_resource = load("res://scripts/room_resources/%s.tres" % room_name) as RoomResource
+	var new_room_scene = new_room_resource.room_scene.instantiate()
+	roomMargin.add_child(new_room_scene)
+	roomButtons = new_room_scene.find_child("roomButtons")
+	getRoomButtons()
+	
+	var new_room_text = new_room_resource.room_text
+	loadText(new_room_text)
+	
+	current_room_resource = new_room_resource
+	
+func unloadCurrentRoom():
+	if roomButtons:
+		for child in roomButtons.get_children():
+			if child is RoomButton:
+				child.mouse_entered.disconnect(updateMouseText)
+				child.mouse_exited.disconnect(clearMouseText)
+				child.pressed.disconnect(roomButtonPressed)
+	roomMargin.get_child(0).queue_free()
+	descriptionText.text = ""
+	clearMouseText()
+	
 func getRoomButtons():
 	for child in roomButtons.get_children():
 		if child is RoomButton:
@@ -22,6 +55,11 @@ func getRoomButtons():
 			child.mouse_entered.connect(updateMouseText.bind(child.hover_text))
 			child.mouse_exited.connect(clearMouseText)
 			child.pressed.connect(roomButtonPressed.bind(child.button_id))
+
+func loadText(t):
+	for x in range(len(t)):
+		descriptionText.text += t[x]
+		await get_tree().create_timer(0.05).timeout
 	
 func updateMouseText(t):
 	print("MOUSE ENTERED BUTTON ", t)
@@ -30,9 +68,18 @@ func updateMouseText(t):
 func clearMouseText():
 	mouseLabel.text = ""
 
-func roomButtonPressed(button_id: String):
-	match button_id:
+func roomButtonPressed(button_event: String):
+	match button_event:
 		"bed": print("do spooky thing")
 		"clock": print("does spookier thing")
-		"window_button_1": print("YOU CLICKED THE WINDOWW")
+		"front_door_button": loadRoom("hospital_test")
+		"hospital_door": loadRoom("front_door_test")
 		_: pass
+		
+func handleFlashlightEvent(event_id):
+	match event_id:
+		"spooky_hospital": loadRoom("hospital_dark_test")
+		"normal_hospital": loadRoom("hospital_test")
+		"hallway_1_light": loadRoom("hallway_1_dark")
+		"hallway_1_dark": loadRoom("hallway_2_light")
+		
---
+++ scripts/room_data.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name RoomResource
+
+@export var room_scene : PackedScene
+@export var room_text : String
+@export var room_flashlight_on_event : String
+@export var room_flashlight_off_event : String
+@export var room_face : String
