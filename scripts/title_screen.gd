6250f8ebb4d5dd5ad4e07b2fc086b37df1cf8bb6
Birdboys <colbyjbird@comcast.net>
2025-10-05 18:48:37 -0600

    fixed title screen

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -4,4 +4,4 @@ var playing = false
 
 func _on_start_button_pressed() -> void:
 	playing = true
-	get_tree().change_scene_to_file("res://scenes/room_scenes/intro_phone.tscn")
+	get_tree().change_scene_to_file("res://scenes/main_ui.tscn")
